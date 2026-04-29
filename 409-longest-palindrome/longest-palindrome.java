8ad1978f8f70fc3a767d2f814a9672ea880982f9
Abhishek Tiwari <abhishek.mtiwari2004@gmail.com>
2026-04-29 23:26:31 +0530

    Time: 1 ms (100.00%) | Memory: 43.1 MB (70.65%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int [] count = new int [128];
+        for(char ch : s.toCharArray()){
+            count[ch]++;
+        }
+        int length=0;
+        for(int freq : count){
+            length = length+(freq/2)*2;
+            if(length %2==0 && freq%2==1){
+                length++;
+            }
+        }
+        return length;
+        
+    }
+}
